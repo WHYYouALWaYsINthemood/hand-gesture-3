87b2a67048a26ce6c4921cfd3b305103dab03b6e
WHYYouALWaYsINthemood <122185449+WHYYouALWaYsINthemood@users.noreply.github.com>
2023-12-12 18:42:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,117 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width: 350, 
+    height: 300, 
+    image_format: 'png', 
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+ data_uri '"/>';
+    });
+}
+
+console.log('ml5 version: ', ml5.version);
+classifier = ml5.imageClassifier('prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width: 350, 
+    height: 300, 
+    image_format: 'png', 
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+ data_uri '"/>';
+    });
+}
+
+console.log('ml5 version: ', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rA8FTShqc/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is" + prediction_1;
+    speak_data_2 = "The second prediction is" + prediction2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is" + prediction_1;
+    speak_data_2 = "The second prediction is" + prediction2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotresult);
+}
+
+function gotresult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else {}
+    {
+        console.log(results);
+        document.getElementById("result_hand_pose_name").innerHTML = results[0].label;
+        document.getElementById("results_hand_pose_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "ok")
+        {
+            document.getElementById("update_hand_pose").innerHTML = "&#128522;";
+        }
+        if(results[0].label == "best")
+        {
+            document.getElementById("update_hand_pose").innerHTML = "&#128532;";
+        }
+        if(results[0].label == "victory")
+        {
+            document.getElementById("update_hand_pose").innerHTML = "&#128548;";
+        }
+        if(results[1].label == "ok")
+        {
+            document.getElementById("update_hand_pose2").innerHTML = "&#128522;";
+        }
+        if(results[1].label == "best")
+        {
+            document.getElementById("update_hand_pose2").innerHTML = "&#128532;";
+        }
+        if(results[1].label == "victory")
+        {
+            document.getElementById("update_hand_pose2").innerHTML = "&#128548;";
+        }
+    }
+}
